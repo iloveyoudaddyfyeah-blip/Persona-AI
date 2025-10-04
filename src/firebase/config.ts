c0f6effc1e75752e901c2635aa91aa60f810d121
Caleb Liskey <calebliskey51@gmail.com>
2025-10-04 00:05:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8369764810-7b30f",
+  "appId": "1:561737266475:web:57836c9471a518e194c16e",
+  "apiKey": "AIzaSyDCkDzEBKme2E7Jp9y09I3hyq1-QU8zSk8",
+  "authDomain": "studio-8369764810-7b30f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "561737266475"
+};
